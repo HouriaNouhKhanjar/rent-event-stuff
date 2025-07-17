4f8928ddbf0c3a6b07e124336c1fb6e14e7ef64f
HouriaNouhKhanjar <gls97048@students.codeinstitute.net>
2025-07-17 11:28:14 +0200

    add start renting date to bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -4,25 +4,44 @@ from django.shortcuts import get_object_or_404
 from supplies.models import Supply
 
 
-def bag_contents(request):
+BAG_KEY = "bag"
+ID_KEY = "id"
+QTY_KEY = "quantity"
+DATE_KEY = "start_renting_date"
+DAYS_KEY = "renting_days"
+
+
+def get_bag_items(bag):
 
     bag_items = []
     total = 0
     supply_count = 0
-    bag = request.session.get('bag', {})
 
     for item_id, item_obj in bag.items():
-        supply = get_object_or_404(Supply, pk=item_id)
-        sub_total = item_obj['quantity'] * item_obj['renting_days'] * supply.price_per_day
-        total += sub_total
-        supply_count += item_obj['quantity']
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': item_obj['quantity'],
-            'renting_days': item_obj['renting_days'],
-            'sub_total': sub_total,
-            'supply': supply,
-        })
+        id = int(item_id)
+        supply = get_object_or_404(Supply, pk=id)
+        for date, days_obj in item_obj.items():
+            for days, quantity_data in days_obj.items():
+                sub_total = int(days) * quantity_data[QTY_KEY] * supply.price_per_day
+                total += sub_total
+                supply_count += quantity_data[QTY_KEY]
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity_data[QTY_KEY],
+                    'renting_days': days,
+                    'start_renting_date': date,
+                    'sub_total': sub_total,
+                    'supply': supply,
+                })
+
+    return bag_items, total, supply_count
+
+
+def bag_contents(request):
+
+    bag = request.session.get(BAG_KEY, {})
+
+    bag_items, total, supply_count = get_bag_items(bag)
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
+BAG_KEY = "bag"
+ID_KEY = "id"
+QTY_KEY = "quantity"
+DATE_KEY = "start_renting_date"
+DAYS_KEY = "renting_days"
 
 
 def view_bag(request):
@@ -10,14 +15,32 @@ def view_bag(request):
 
 
 def check_item_in_bag(item, bag):
-    """ Check if item already exist in the shoping bag"""
+    """Check if item already exist in the shoping bag"""
 
-    supply_id = item['id']
-    del item['id']
+    updated = False
+
+    supply_id = item[ID_KEY]
+    del item[ID_KEY]
+    renting_date = item[DATE_KEY]
+    days = item[DAYS_KEY]
+
     if supply_id in list(bag.keys()):
-        bag[supply_id]['quantity'] += item['quantity']
+        bag_item = bag[supply_id]
+        if renting_date in list(bag_item.keys()):
+            if days in list(bag_item[renting_date].keys()):
+                bag[supply_id][renting_date][days][QTY_KEY] += item[QTY_KEY]
+                updated = True
+        else:
+            bag[supply_id].update({renting_date: {}})
+
+        if not updated:
+            bag[supply_id][renting_date].update({days: {}})
+            bag[supply_id][renting_date][days].update({QTY_KEY: item[QTY_KEY]})
     else:
-        bag[supply_id] = item
+        bag.update({supply_id: {}})
+        bag[supply_id].update({renting_date: {}})
+        bag[supply_id][renting_date].update({days: {}})
+        bag[supply_id][renting_date][days].update({QTY_KEY: item[QTY_KEY]})
 
 
 def add_to_bag(request, item_id):
@@ -26,12 +49,14 @@ def add_to_bag(request, item_id):
     shopping bag """
 
     item = {}
-    item['id'] = item_id
-    item['quantity'] = int(request.POST.get('quantity'))
-    item['renting_days'] = int(request.POST.get('renting_days'))
+    item[ID_KEY] = item_id
+    item[QTY_KEY] = int(request.POST.get(QTY_KEY))
+    item[DAYS_KEY] = int(request.POST.get(DAYS_KEY))
+    item[DATE_KEY] = request.POST.get(DATE_KEY)
     redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('bag', {})
+    bag = request.session.get(BAG_KEY, {})
     check_item_in_bag(item, bag)
 
-    request.session['bag'] = bag
+    print(bag)
+    request.session[BAG_KEY] = bag
     return redirect(redirect_url)
