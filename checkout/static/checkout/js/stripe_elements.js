8b29a295b4b5a938be25f0dca242a8ce136eee98
HouriaNouhKhanjar <gls97048@students.codeinstitute.net>
2025-07-19 20:23:29 +0200

    added basic checkout functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -5,43 +5,75 @@
     CSS from here: 
     https://stripe.com/docs/stripe-js
 */
+var stripePublic_key = $("#id_stripe_public_key").text().slice(1, -1);
+var clientSecret = $("#id_client_secret").text().slice(1, -1);
+var stripe = Stripe(stripePublic_key);
+var elements = stripe.elements();
+var style = {
+  base: {
+    color: "#000",
+    fontFamily: '"Open Sans", sans-serif',
+    fontSmoothing: "antialiased",
+    fontSize: "16px",
+    "::placeholder": {
+      color: "#6c7581"
+    }
+  },
+  invalid: {
+    color: "#dc3545",
+    iconColor: "#dc3545"
+  }
+};
 
 function getStripeOneTime(containerClass) {
-  var stripe_public_key = $("#id_stripe_public_key").text().slice(1, -1);
-  var client_secret = $("#id_stripe_public_key").text().slice(1, -1);
-  var stripe = Stripe(stripe_public_key);
-  var elements = stripe.elements();
-  var style = {
-    base: {
-      color: "#000",
-      fontFamily: '"Open Sans", sans-serif',
-      fontSmoothing: "antialiased",
-      fontSize: "16px",
-      "::placeholder": {
-        color: "#6c7581"
-      },
-    },
-    invalid: {
-      color: "#dc3545",
-      iconColor: "#dc3545"
-    }
-  };
   var card = elements.create("card", { style: style });
   card.mount(`.${containerClass} .card-element`);
   // Handle realtime validation errors on the card element
-  card.addEventListener('change', function (event) {
-        var errorDiv = document.querySelector(`.${containerClass} .card-errors`);
-        if (event.error) {
-            var html = `
+  card.addEventListener("change", function (event) {
+    var errorDiv = document.querySelector(`.${containerClass} .card-errors`);
+    if (event.error) {
+      var html = `
                 <span class="icon" role="alert">
                     <i class="fas fa-times"></i>
                 </span>
                 <span>${event.error.message}</span>`;
-            $(errorDiv).html(html);
-        } else {
-            errorDiv.textContent = '';        
+      $(errorDiv).html(html);
+    } else {
+      errorDiv.textContent = "";
+    }
+  });
+
+  // Handle form submit
+  var form = document.querySelector(`.${containerClass} .payment-form`);
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    card.update({ disabled: true });
+    $(`.${containerClass} .submit-payment-form`).attr("disabled", true);
+    stripe
+      .confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card
         }
-    });
+      })
+      .then(function (result) {
+        if (result.error) {
+          var errorDiv = document.querySelector(`.${containerClass} .card-errors`);;
+          var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+          $(errorDiv).html(html);
+          card.update({ disabled: false });
+          $(`.${containerClass} .submit-payment-form`).attr("disabled", false);
+        } else {
+          if (result.paymentIntent.status === "succeeded") {
+            form.submit();
+          }
+        }
+      });
+  });
 }
 
 window.addEventListener("resize", function (event) {
@@ -56,12 +88,12 @@ window.addEventListener("resize", function (event) {
   }
 });
 
-  if (document.body.clientWidth >= 992) {
-    if ($(".checkout-desktop .card-element").children().length == 0) {
-      getStripeOneTime("checkout-desktop");
-    }
-  } else {
-    if ($(".checkout-mobile .card-element").children().length == 0) {
-      getStripeOneTime("checkout-mobile");
-    }
+if (document.body.clientWidth >= 992) {
+  if ($(".checkout-desktop .card-element").children().length == 0) {
+    getStripeOneTime("checkout-desktop");
   }
+} else {
+  if ($(".checkout-mobile .card-element").children().length == 0) {
+    getStripeOneTime("checkout-mobile");
+  }
+}
--- checkout/views.py
+++ checkout/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
 from user_profile.forms import AddressForm
 from bag.contexts import bag_contents
-
 import stripe
 
 
 def checkout(request):
     """view to display the checkout page if the bag i not empty"""
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -18,14 +22,25 @@ def checkout(request):
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
 
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
     address_form = AddressForm()
     template = 'checkout/checkout.html'
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     context = {
         'order_form': order_form,
         'address_form': address_form,
-        'stripe_public_key': 'pk_test_51RmKsi097A1V6ZA75q4vDrhCiYIIU6igAJHdaEkfjYOjzWiJ3vAY9JHvJHV4yV4wx8UQrXIALujOgF1mEDHKWPSJ00OngrIQ3w',
-        'client_secret': 'this is my secrete key',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
