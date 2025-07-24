e77bf3c2031202ffeea844b66bcc9b46e6976859
HouriaNouhKhanjar <gls97048@students.codeinstitute.net>
2025-07-24 13:02:30 +0200

    change debugmode in settings

--- rent_event_stuff/settings.py
+++ rent_event_stuff/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -132,7 +132,7 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-else: 
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
