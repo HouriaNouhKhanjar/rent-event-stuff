861e0a15b2478b148e7e0e9669143c0ef0980651
HouriaNouhKhanjar <gls97048@students.codeinstitute.net>
2025-07-24 12:41:59 +0200

    setup deploy

--- rent_event_stuff/settings.py
+++ rent_event_stuff/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 if os.path.isfile('env.py'):
@@ -133,7 +134,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
