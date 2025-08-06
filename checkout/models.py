57d5149d007d4a62447ea7b13f5ff605fda54d71
HouriaNouhKhanjar <gls97048@students.codeinstitute.net>
2025-08-06 10:30:47 +0200

    fix header style on base template and sort orders on created on

--- checkout/models.py
+++ checkout/models.py
@@ -40,6 +40,9 @@ class Order(models.Model):
     stripe_pid = models.CharField(max_length=254, null=False, blank=False,
                                   default='')
 
+    class Meta:
+        ordering = ['-created_on']
+
     def _generate_order_number(self):
         """
         Generate a random, unique order number using UUID
